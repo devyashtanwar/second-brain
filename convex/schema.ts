5dbdd1da206f05f1497a3ad6cf3af94be40ab7d9
devyash_tanwar <tanwardevyash@gmail.com>
2024-10-07 21:56:18 +0530

    Scoping data to user identifier

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,5 +4,8 @@ import { v } from "convex/values";
 export default defineSchema({
     // Other tables here...
 
-    documents: defineTable({ title: v.string() }),
+    documents: defineTable({
+        title: v.string(),
+        tokenIdentifier: v.string(),
+    }).index("by_tokenIdentifier", ["tokenIdentifier"]),
 });
